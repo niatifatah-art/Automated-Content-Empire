096476c33c74a78fd139db77acefc4072a9fe6f6
Niati Fatah <niatifatah@gmail.com>
2026-07-06 20:19:58 +0100

    Add basic CLI with argparse

--- src/ace/main.py
+++ src/ace/main.py
@@ -1,4 +1,20 @@
+import argparse
+
+
 def main():
+    parser = argparse.ArgumentParser(
+        prog="ace",
+        description="Automated Content Empire"
+    )
+
+    parser.add_argument(
+        "--version",
+        action="version",
+        version="ACE 0.1.0"
+    )
+
+    args = parser.parse_args()
+
     print("Welcome to Automated Content Empire (ACE)")
 
 
